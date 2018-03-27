6dadda480e2ac6bb35a29fde5329c7ad847a506a
serah chepkirui <serahkaku254@gmail.com>
2018-03-27 15:58:45 +0300

    added the testcases

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ def setUp(self):
            "Pages":500,
 	
         }
-        #test whether the api can add a book"""
+        #test whether the api can add a book
 
         
 def test_add_book(self):
@@ -22,7 +22,7 @@ def test_add_book(self):
       response = self.client.post('/api/books/', data=self.book)
       self.assertEqual(response.status_code, 201)
       self.assertIn('coding', str(response.data))
-#test whether the api can retrieve a book by id"""
+#test whether the api can get  a book by id"""
 def test_get_book_by_id(self):
         
         response = self.client.post('/api/books/', data=self.book)
@@ -31,7 +31,35 @@ def test_get_book_by_id(self):
         response_get = self.client.get('/api/books/{}'.format(json_response['book-id']))
         self.assertEqual(response_get, 200)
         self.assertIn('coding', response_get.data)
+def test_update_book(self):
+       #test whether the api can update book information"""
 
+    response_post = self.client.post('/api/books/', data=self.book)
+    self.assertEqual(response_post.status_code, 201)
+    book = self.book['publisher'] = 'serah'
+    response_put = self.client.put('/api/books/13', data=book)
+    self.assertEqual(response_put.status_code, 200)
+    response_get = self.client.get('/api/books/13')
+    self.assertIn('coding', str(response_get.data))
+
+    def test_get_all_books(self):
+        #test whether the api can retrieve all books
+
+        response_post = self.client.post('/api/books/', data=self.book)
+        self.assertEqual(response_post.status_code, 201)
+        response_get = self.client.get('/api/books/')
+        self.assertEqual(response_get.status_code, 200)
+        self.assertIn('coding', str(response_get))
+
+    def test_delete_book(self):
+        #test whether the api can delete a book"""
+
+        response_post = self.client.post('/api/books/', data=self.book)
+        self.assertEqual(response_post.status_code, 201)
+        response_delete = self.client.delete('api/books/13')
+        self.assertEqual(response_delete.status_code, 200)
+        response_get = response_get = self.client.get('/api/books/13')
+        self.assertEqual(response_get.status_code, 404)
   
 if __name__ == '__main__':
    unittest.main()
